65fe34f5da8a1d8f40406f1389fb39bbd03bf3f8
Prin-S <98200861+Prin-S@users.noreply.github.com>
2024-11-03 17:47:31 +0700

    Added getShipLength() to ship().

--- src/ship.js
+++ src/ship.js
@@ -2,6 +2,7 @@ function ship(length) {
     let timesHit = 0;
     let sunk = false;
 
+    const getShipLength = () => length;
     const hit = () => timesHit += 1;
     const isSunk = () => {
         if (length - timesHit <= 0) {
@@ -11,7 +12,7 @@ function ship(length) {
         }
     }
 
-    return { hit, isSunk };
+    return { getShipLength, hit, isSunk };
 }
 
 export { ship };
