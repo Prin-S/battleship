f083a1cbda47f8a32ce00bc92b9a5618df315d98
Prin-S <98200861+Prin-S@users.noreply.github.com>
2024-11-03 17:49:01 +0700

    Added gameboard() and tests for it.
    gameboard() is a factory function.
    gameboard() creates an empty 10 x 10 gameboard each time it is called.
    Added placeShip() in gameboard() to allow placing of ships on the gameboard.

--- src/battleship.test.js
+++ src/battleship.test.js
@@ -1,8 +1,13 @@
 import { ship } from './ship.js';
+import { gameboard } from './gameboard.js';
 
 describe('ship functions', () => {
     describe('ship one: length = 5', () => {
         const testShip1 = ship(5);
+
+        test('getShipLength()', () => {
+            expect(testShip1.getShipLength()).toBe(5);
+        });
 
         test('hit() once', () => {
             expect(testShip1.hit()).toBe(1);
@@ -16,6 +21,10 @@ describe('ship functions', () => {
     describe('ship two: length = 2', () => {
         const testShip2 = ship(2);
 
+        test('getShipLength()', () => {
+            expect(testShip2.getShipLength()).toBe(2);
+        });
+
         test('hit() two times', () => {
             expect(testShip2.hit()).toBe(1);
             expect(testShip2.hit()).toBe(2);
@@ -24,5 +33,94 @@ describe('ship functions', () => {
         test('isSunk(): true', () => {
             expect(testShip2.isSunk()).toBeTruthy();
         });
+    });
+});
+
+describe('gameboard functions', () => {
+    let testGameboard = gameboard();
+    
+    beforeEach(() => {
+        testGameboard = gameboard();
+    });
+
+    test('getBoard()', () => {
+        expect(testGameboard.getBoard()).toEqual(new Array(10).fill(new Array(10).fill(0)));
+    });
+
+    test('placeShip() - invalid ship length', () => {
+        expect(testGameboard.placeShip(-1, 0, 0, 'horizontal')).toBe('The ship length must be from 0 to 5.');
+    });
+
+    test('placeShip() - invalid ship length', () => {
+        expect(testGameboard.placeShip(6, 0, 0, 'horizontal')).toBe('The ship length must be from 0 to 5.');
+    });
+
+    test('placeShip() - invalid X grid position', () => {
+        expect(testGameboard.placeShip(2, -1, 0, 'horizontal')).toBe('The grid position must be from 0 to 9.');
+    });
+
+    test('placeShip() - invalid Y grid position', () => {
+        expect(testGameboard.placeShip(2, 0, 10, 'horizontal')).toBe('The grid position must be from 0 to 9.');
+    });
+
+    test('placeShip() - invalid X and Y grid position', () => {
+        expect(testGameboard.placeShip(2, -1, 10, 'horizontal')).toBe('The grid position must be from 0 to 9.');
+    });
+
+    test('placeShip() horizontal', () => {
+        expect(testGameboard.placeShip(5, 2, 3, 'horizontal')).toEqual([
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 1, 1, 1, 1, 1, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        ]);
+    });
+
+    test('placeShip() horizontal - invalid', () => {
+        expect(testGameboard.placeShip(5, 6, 0, 'horizontal')).toBe('Invalid position - the ship will fall outside the gameboard.');
+    });
+
+    test('placeShip() horizontal - invalid', () => {
+        expect(testGameboard.placeShip(5, 6, 9, 'horizontal')).toBe('Invalid position - the ship will fall outside the gameboard.');
+    });
+
+    test('placeShip() vertical', () => {
+        expect(testGameboard.placeShip(4, 5, 1, 'vertical')).toEqual([
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        ]);
+    });
+
+    test('placeShip() vertical - invalid', () => {
+        expect(testGameboard.placeShip(5, 0, 6, 'vertical')).toBe('Invalid position - the ship will fall outside the gameboard.');
+    });
+
+    test('placeShip() vertical - invalid', () => {
+        expect(testGameboard.placeShip(5, 9, 6, 'vertical')).toBe('Invalid position - the ship will fall outside the gameboard.');
+    });
+
+    test('placeShip() two times - at least one square is occupied', () => {
+        testGameboard.placeShip(5, 2, 3, 'horizontal');
+        expect(testGameboard.placeShip(4, 5, 1, 'vertical')).toBe('Invalid position - at least one square is occupied.');
+    });
+
+    test('placeShip() two times and then getShips()', () => {
+        testGameboard.placeShip(5, 2, 3, 'horizontal');
+        testGameboard.placeShip(4, 8, 1, 'vertical');
+        expect(testGameboard.getShips()).toEqual([5, 4]);
     });
 });
---
+++ src/gameboard.js
@@ -0,0 +1,58 @@
+import { ship } from './ship.js';
+
+function gameboard() {
+    const board = new Array(10).fill(0).map(() => new Array(10).fill(0)); // Create a 10 x 10 array for the gameboard.
+    // First, create an array with 10 elements and fill each element with 0.
+    // Second, map each element with a new array filled with 0s.
+    // If new Array(10).fill(new Array(10).fill(0)); is used, each element will have the same array,
+    // so when something is changed in one, it will affect all elements.
+    
+    const ships = [];
+
+    const getBoard = () => board;
+    const getShips = () => ships;
+
+    const placeShip = (shipLength, startingX, startingY, direction) => {
+        ships.push(ship(shipLength).getShipLength());
+
+        if (shipLength < 1 || shipLength > 5) {
+            return 'The ship length must be from 0 to 5.';
+        }
+        
+        if (startingX < 0 || startingX > 9 || startingY < 0 || startingY > 9) {
+            return 'The grid position must be from 0 to 9.';
+        }
+
+        if (direction == 'horizontal') {
+            if (startingX + shipLength > 10) {
+                return 'Invalid position - the ship will fall outside the gameboard.';
+            }
+
+            for (let i = 0; i < shipLength; i++) {
+                if (board[startingY][startingX + i] == 1) {
+                    return 'Invalid position - at least one square is occupied.';
+                }
+
+                board[startingY][startingX + i] = 1;
+            }
+        } else {
+            if (startingY + shipLength > 10) {
+                return 'Invalid position - the ship will fall outside the gameboard.';
+            }
+
+            for (let i = 0; i < shipLength; i++) {
+                if (board[startingY + i][startingX] == 1) {
+                    return 'Invalid position - at least one square is occupied.';
+                }
+
+                board[startingY + i][startingX] = 1;
+            }
+        }
+
+        return board;
+    }
+
+    return { getBoard, getShips, placeShip };
+}
+
+export { gameboard };
