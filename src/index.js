a1574f2b43d20683876ea33cabd5cfdeb42c65ed
Prin-S <98200861+Prin-S@users.noreply.github.com>
2024-11-09 17:04:22 +0700

    Added turns and a check to end the game.
    The board border color changes to show the current turn.
    Added a hover effect to the computer player's board.
    Simplified some code.
    Changed the interface layout.

--- src/index.js
+++ src/index.js
@@ -3,17 +3,23 @@ import { ship } from './ship.js';
 import { gameboard } from './gameboard.js';
 import { player } from './player.js';
 
+const currTurn = document.querySelector('#turn-box');
+let currTurnCode = 'hum';
+let won = false;
+
 const humPlayer = player('com');
+const humBox = document.querySelector('#box1');
 const humBoard = document.querySelector('#human-board');
-const box2Text1 = document.querySelector('#box2-text1');
-const box2Text2 = document.querySelector('#box2-text2');
 
 const comPlayer = player('com');
+const comBox = document.querySelector('#box2');
 const comBoard = document.querySelector('#computer-board');
-const box4Text1 = document.querySelector('#box4-text1');
-const box4Text2 = document.querySelector('#box4-text2');
 
-function createBoard(player, board, type, boxText1, boxText2) {
+const box3Text1 = document.querySelector('#box3-text1');
+const box3Text2 = document.querySelector('#box3-text2');
+const box3Text3 = document.querySelector('#box3-text3');
+
+function createBoard(board, type) {
     for (let i = 0; i < 10; i++) {
         for (let j = 0; j < 10; j++) {
             const square = document.createElement('div');
@@ -26,20 +32,24 @@ function createBoard(player, board, type, boxText1, boxText2) {
             square.style['grid-row-start'] = i + 1;
             square.style['grid-row-end'] = i + 2;
 
-            square.addEventListener('click', clickSquare.bind(this, player, type, boxText1, boxText2, i, j));
+            square.addEventListener('click', clickSquare.bind(this, type, i, j));
             
-            if (type == 'hum') { // Only reveal the ship positions for the human player's board.
+            if (type == 'hum') { // Only reveal ship positions for the human player's board.
                 if (humPlayer.getGameboard()[i][j] == 1) {
-                    square.setAttribute('class', 'square five-square-ship');
+                    square.classList.add('five-square-ship');
                 } else if (humPlayer.getGameboard()[i][j] == 2) {
-                    square.setAttribute('class', 'square four-square-ship');
+                    square.classList.add('four-square-ship');
                 } else if (humPlayer.getGameboard()[i][j] == 3) {
-                    square.setAttribute('class', 'square three-square-ship-1');
+                    square.classList.add('three-square-ship-1');
                 } else if (humPlayer.getGameboard()[i][j] == 4) {
-                    square.setAttribute('class', 'square three-square-ship-2');
+                    square.classList.add('three-square-ship-2');
                 } else if (humPlayer.getGameboard()[i][j] == 5) {
-                    square.setAttribute('class', 'square two-square-ship');
+                    square.classList.add('two-square-ship');
                 }
+            }
+
+            if (type == 'com') { // Only show the hover effect for the computer player's board.
+                square.classList.add('hoverable');
             }
             
             board.appendChild(square);
@@ -47,18 +57,44 @@ function createBoard(player, board, type, boxText1, boxText2) {
     }
 }
 
-function clickSquare(player, type, boxText1, boxText2, x, y) {
-    const currSquare = document.getElementById(`${type}-square-${x}-${y}`);
+function clickSquare(type, x, y) {
+    if (currTurnCode != type && !won) {
+        const currSquare = document.getElementById(`${type}-square-${x}-${y}`);
 
-    boxText1.innerHTML = `(${x}, ${y}) selected.`;
-    boxText2.innerHTML = `${player.checkHit(y, x)}`;
+        let currPlayer;
 
-    if (boxText2.innerHTML == 'Miss!') {
-        currSquare.setAttribute('class', 'square miss');
-    } else if (boxText2.innerHTML == 'Hit!' || boxText2.innerHTML == 'Hit - ship sunk!' || boxText2.innerHTML == 'Hit - all ships sunk!') {
-        currSquare.setAttribute('class', 'square hit');
+        if (currTurn.innerHTML == 'Your turn') {
+            currTurn.innerHTML = `Computer's turn`;
+            currTurnCode = 'com';
+            humBox.classList.add('current-turn-box');
+            comBox.classList.remove('current-turn-box');
+            currPlayer = comPlayer;
+        } else {
+            currTurn.innerHTML = 'Your turn';
+            currTurnCode = 'hum';
+            humBox.classList.remove('current-turn-box');
+            comBox.classList.add('current-turn-box');
+            currPlayer = humPlayer;
+        }
+
+        box3Text1.innerHTML = `(${x}, ${y}) selected.`;
+        box3Text2.innerHTML = `${currPlayer.checkHit(y, x)}`;
+
+        if (box3Text2.innerHTML == 'Miss!') {
+            currSquare.setAttribute('class', 'square miss');
+        } else if (box3Text2.innerHTML == 'Hit!' || box3Text2.innerHTML == 'Hit - ship sunk!' || box3Text2.innerHTML == 'Hit - all ships sunk!') {
+            currSquare.setAttribute('class', 'square hit');
+        }
+
+        if (box3Text2.innerHTML == 'Hit - all ships sunk!' && currTurnCode == 'com') {
+            won = true;
+            box3Text3.innerHTML = 'You won!';
+        } else if (box3Text2.innerHTML == 'Hit - all ships sunk!' && currTurnCode == 'hum') {
+            won = true;
+            box3Text3.innerHTML = 'The computer won!';
+        }
     }
 }
 
-createBoard(humPlayer, humBoard, 'hum', box2Text1, box2Text2);
-createBoard(comPlayer, comBoard, 'com', box4Text1, box4Text2);
+createBoard(humBoard, 'hum');
+createBoard(comBoard, 'com');
